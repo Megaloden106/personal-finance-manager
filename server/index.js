d13402d66dcdc6940876335677cda829f1acd83c
EddieLo <eddielo106@gmail.com>
2019-04-09 17:37:10 -0700

    build: setup webpack/eslint alias
    - setup alias for root and client directories
    - setup intial webpack build and serve with index.html
    - setup front end file structure for redux and router

--- server/index.js
+++ server/index.js
@@ -4,17 +4,18 @@ const bodyParser = require('body-parser');
 const compression = require('compression');
 
 const app = express();
-const router = require('./routes.js');
+const router = require('./config/routes.js');
 
 app.use(compression());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 
-app.use('/', express.static('punpmblic'));
+app.use('/', express.static('../public'));
+app.use('/static', express.static('assets'));
 
 app.use('/api', router);
 
-app.set('port', process.env.PORT || 7070);
+app.set('port', process.env.PORT || 8080);
 
 app.listen(app.get('port'), () => {
   console.warn(`app is listening to port ${app.get('port')}`);
