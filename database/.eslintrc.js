79d410994d869404e1614927c76075029464c869
EddieLo <eddielo106@gmail.com>
2019-05-15 21:36:38 -0700

    refactor: Modify databasse to typescript
    Close PFMS-17.

--- database/.eslintrc.js
+++ database/.eslintrc.js
@@ -1,8 +1,33 @@
+const path = require('path');
+
+const tsconfig = path.resolve(__dirname, 'tsconfig.json');
+
 module.exports = {
-  extends: "airbnb",
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: tsconfig,
+    tsconfigRootDir: "."
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "airbnb",
+    "plugin:@typescript-eslint/recommended",
+  ],
   rules: {
-    "no-console": ["error", { allow: ["warn", "error"] }],
+    "no-console": ["error", { "allow": ["warn", "error"] }],
     "no-underscore-dangle": ["error", { "allowAfterThis": true }],
+    "lines-between-class-members": ["error", "always", { "exceptAfterSingleLine": true }],
+
+    // TS RULES
+    "@typescript-eslint/indent": ["error", 2],
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        paths: ["./src", "./src/style"],
+        extensions: [".js", ".jsx", ".ts", ".tsx"]
+      },
+    },
   },
   env: {
     browser: true,
