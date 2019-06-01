f64b10d6f75540ce4fd19b01c272fbe2bfc47702
Eddie Lo <40453061+Megaloden106@users.noreply.github.com>
2019-05-31 22:55:42 -0700

    feat: Scaffolding for Portfolio page (#16)
    Closes PFMS-4.

---
+++ client/components/NavBar/Base/BaseNavBar.tsx
@@ -0,0 +1,12 @@
+import React, { SFC } from 'react';
+import styles from './BaseNavBar.scss';
+
+const BaseNavBar: SFC = () => (
+  <div className={styles.container}>
+    <div className={styles.content}>
+      NAV BAR
+    </div>
+  </div>
+);
+
+export default BaseNavBar;
---
+++ client/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,10 @@
+import React, { SFC } from 'react';
+import styles from './Sidebar.scss';
+
+const Sidebar: SFC = () => (
+  <div className={styles.container}>
+    SIDEBAR
+  </div>
+);
+
+export default Sidebar;
